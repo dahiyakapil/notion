24a2d80b39b6b9faca2b9a76da186bd9912eb3aa
KAPIL DAHIYA <kapildahiya309@gmail.com>
2025-09-18 16:02:10 +0530

    fixed

--- api/notion.js
+++ api/notion.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
       {
         method: "POST",
         headers: {
-          "Authorization": `Bearer ${process.env.NOTION_API_KEY}`,
+          "Authorization": `Bearer ${process.env.NOTION_API_TOKEN}`,
           "Notion-Version": "2022-06-28",
           "Content-Type": "application/json",
         },
